572db3ae35ef8b17dc3a5fa568e2e2149c792113
feichai0017 <songguocheng348@gmail.com>
2025-10-14 23:18:16 +1100

    feat: update new batchwrite optimization

--- db.go
+++ db.go
@@ -36,6 +36,9 @@ type (
 		stats       *Stats
 		flushChan   chan flushTask // For flushing memtables.
 		writeCh     chan *request
+		batchCh     chan *writeBatch
+		applyCh 	chan *writeBatch
+		writeCloser *utils.Closer
 		blockWrites int32
 		vhead       *utils.ValuePtr
 		logRotates  int32
@@ -43,17 +46,31 @@ type (
 		orc         *oracle
 		hot         *hotring.HotRing
 	}
+	
+	writeBatch struct {
+		reqs 	[]*request
+		entries int
+		size 	int64
+	}
 )
 
 var (
 	head = []byte("!NoKV!head") // For storing value offset for replay.
 )
 
+
+const (
+	defaultWriteBatchMaxCount 	= 64
+	defaultWriteBatchMaxSize 	= 1 << 20
+	defaultWriteBatchDelay 		= 2 * time.Millisecond
+)
+
 // Open DB
 // TODO 这里是不是要上一个目录锁比较好，防止多个进程打开同一个目录?
 func Open(opt *Options) *DB {
 	c := utils.NewCloser()
-	db := &DB{opt: opt}
+	db := &DB{opt: opt, writeCloser: c}
+	db.initWriteBatchOptions()
 
 	wlog, err := wal.Open(wal.Config{
 		Dir: opt.WorkDir,
@@ -95,6 +112,8 @@ func Open(opt *Options) *DB {
 	// 准备vlog gc
 	c.Add(1)
 	db.writeCh = make(chan *request)
+	db.batchCh = make(chan *writeBatch, 4)
+	db.applyCh = make(chan *writeBatch, 4)
 	db.flushChan = make(chan flushTask, 16)
 	go db.doWrites(c)
 	// 启动 info 统计过程
@@ -103,6 +122,9 @@ func Open(opt *Options) *DB {
 }
 
 func (db *DB) Close() error {
+	if db.writeCloser != nil {
+		db.writeCloser.Close()
+	}
 	db.vlog.lfDiscardStats.closer.Close()
 	if err := db.lsm.Close(); err != nil {
 		return err
@@ -305,64 +327,104 @@ func (db *DB) batchSet(entries []*utils.Entry) error {
 
 func (db *DB) doWrites(lc *utils.Closer) {
 	defer lc.Done()
-	pendingCh := make(chan struct{}, 1)
-
-	writeRequests := func(reqs []*request) {
-		if err := db.writeRequests(reqs); err != nil {
-			utils.Err(fmt.Errorf("writeRequests: %v", err))
+	lc.Add(2)
+	go db.processValueLogBatches(lc)
+	go db.applyBatches(lc)
+	
+	reqLen := new(expvar.Int)
+	
+	var (
+		pending      []*request
+		pendingCount int
+		pendingSize  int64
+		maxCount     = db.opt.WriteBatchMaxCount
+		maxSize      = db.opt.WriteBatchMaxSize
+		delay        = db.opt.WriteBatchDelay
+		timer        *time.Timer
+		timerActive  bool
+	)
+	resetTimer := func() {
+		if delay <= 0 {
+			return
 		}
-		<-pendingCh
+		if timer == nil {
+			timer = time.NewTimer(delay)
+		} else {
+			if !timer.Stop() {
+				select {
+				case <-timer.C:
+				default:
+				}
+			}
+			timer.Reset(delay)
+		}
+		timerActive = true
 	}
 
-	// This variable tracks the number of pending writes.
-	reqLen := new(expvar.Int)
-
-	reqs := make([]*request, 0, 10)
-	for {
-		var r *request
-		select {
-		case r = <-db.writeCh:
-		case <-lc.CloseSignal:
-			goto closedCase
+	stopTimer := func() {
+		if delay <= 0 || !timerActive || timer == nil {
+			return
 		}
-
-		for {
-			reqs = append(reqs, r)
-			reqLen.Set(int64(len(reqs)))
-
-			if len(reqs) >= 3*utils.KVWriteChCapacity {
-				pendingCh <- struct{}{} // blocking.
-				goto writeCase
-			}
-
+		if !timer.Stop() {
 			select {
-			// Either push to pending, or continue to pick from writeCh.
-			case r = <-db.writeCh:
-			case pendingCh <- struct{}{}:
-				goto writeCase
-			case <-lc.CloseSignal:
-				goto closedCase
-			}
-		}
-
-	closedCase:
-		// All the pending request are drained.
-		// Don't close the writeCh, because it has be used in several places.
-		for {
-			select {
-			case r = <-db.writeCh:
-				reqs = append(reqs, r)
+			case <-timer.C:
 			default:
-				pendingCh <- struct{}{} // Push to pending before doing a write.
-				writeRequests(reqs)
-				return
 			}
 		}
+		timerActive = false
+	}
 
-	writeCase:
-		go writeRequests(reqs)
-		reqs = make([]*request, 0, 10)
+	flush := func() {
+		if len(pending) == 0 {
+			return
+		}
+		batch := &writeBatch{
+			reqs:    pending,
+			entries: pendingCount,
+			size:    pendingSize,
+		}
+		db.batchCh <- batch
+		pending = nil
+		pendingCount = 0
+		pendingSize = 0
 		reqLen.Set(0)
+		stopTimer()
+	}
+
+	for {
+		var timerC <-chan time.Time
+		if timerActive && timer != nil {
+			timerC = timer.C
+		}
+
+		select {
+		case r := <-db.writeCh:
+			if r == nil {
+				continue
+			}
+			pending = append(pending, r)
+			for _, e := range r.Entries {
+				pendingSize += int64(e.EstimateSize(int(db.opt.ValueThreshold)))
+			}
+			pendingCount += len(r.Entries)
+			reqLen.Set(int64(len(pending)))
+
+			if len(pending) == 1 {
+				resetTimer()
+			}
+
+			if (maxCount > 0 && pendingCount >= maxCount) || (maxSize > 0 && pendingSize >= maxSize) || len(pending) >= 3*utils.KVWriteChCapacity {
+				flush()
+			}
+		case <-timerC:
+			timerActive = false
+			flush()
+		case <-lc.CloseSignal:
+			stopTimer()
+			flush()
+			close(db.batchCh)
+			return
+		}
 	}
 }
 
@@ -377,24 +439,16 @@ func (db *DB) getMemTables() ([]*lsm.MemTable, func()) {
 }
 
 // writeRequests is called serially by only one goroutine.
-func (db *DB) writeRequests(reqs []*request) error {
-	if len(reqs) == 0 {
+func (db *DB) applyWriteBatch(batch *writeBatch) error {
+	if batch == nil || len(batch.reqs) == 0 {
 		return nil
 	}
 
 	done := func(err error) {
-		for _, r := range reqs {
-			r.Err = err
-			r.Wg.Done()
-		}
-	}
-	err := db.vlog.write(reqs)
-	if err != nil {
-		done(err)
-		return err
+		completeRequests(batch.reqs, err)
 	}
 	var count int
-	for _, b := range reqs {
+	for _, b := range batch.reqs {
 		if len(b.Entries) == 0 {
 			continue
 		}
@@ -410,6 +464,56 @@ func (db *DB) writeRequests(reqs []*request) error {
 	done(nil)
 	return nil
 }
+
+func (db *DB) processValueLogBatches(lc *utils.Closer) {
+	defer lc.Done()
+	for batch := range db.batchCh {
+		if len(batch.reqs) == 0 {
+			continue
+		}
+		if err := db.vlog.write(batch.reqs); err != nil {
+			completeRequests(batch.reqs, err)
+			continue
+		}
+		db.applyCh <- batch
+	}
+	close(db.applyCh)
+}
+
+func (db *DB) applyBatches(lc *utils.Closer) {
+	defer lc.Done()
+	for batch := range db.applyCh {
+		if err := db.applyWriteBatch(batch); err != nil {
+			utils.Err(fmt.Errorf("applyWriteBatch: %v", err))
+		}
+	}
+}
+
+func completeRequests(reqs []*request, err error) {
+	for _, r := range reqs {
+		if r == nil {
+			continue
+		}
+		r.Err = err
+		r.Wg.Done()
+	}
+}
+
+func (db *DB) initWriteBatchOptions() {
+	if db.opt.WriteBatchMaxCount <= 0 {
+		db.opt.WriteBatchMaxCount = defaultWriteBatchMaxCount
+	}
+	if db.opt.WriteBatchMaxSize <= 0 {
+		db.opt.WriteBatchMaxSize = defaultWriteBatchMaxSize
+	}
+	if db.opt.WriteBatchDelay < 0 {
+		db.opt.WriteBatchDelay = 0
+	}
+	if db.opt.WriteBatchDelay == 0 {
+		db.opt.WriteBatchDelay = defaultWriteBatchDelay
+	}
+}
+
 func (db *DB) writeToLSM(b *request) error {
 	if len(b.Ptrs) != len(b.Entries) {
 		return errors.Errorf("Ptrs and Entries don't match: %+v", b)
