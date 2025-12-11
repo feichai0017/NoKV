177265aab709b4b6f663cfdb1ebf7a0c8a4a1bb3
eric_song <songguocheng348@gmail.com>
2025-12-12 00:07:13 +0800

    Alias GRPC transport metrics to internal metrics

--- raftstore/transport/metrics.go
+++ raftstore/transport/metrics.go
@@ -6,6 +6,8 @@ import (
 	"sync"
 	"sync/atomic"
 	"time"
+
+	"github.com/feichai0017/NoKV/internal/metrics"
 )
 
 const (
@@ -13,21 +15,7 @@ const (
 )
 
 // GRPCTransportMetrics captures gRPC transport watchdog counters exposed via expvar.
-type GRPCTransportMetrics struct {
-	DialsTotal               int64
-	DialFailures             int64
-	SendAttempts             int64
-	SendSuccesses            int64
-	SendFailures             int64
-	Retries                  int64
-	RetryExhausted           int64
-	BlockedPeers             int64
-	LastFailureUnix          int64
-	WatchdogActive           bool
-	WatchdogConsecutiveFails int64
-	WatchdogThreshold        int64
-	WatchdogReason           string
-}
+type GRPCTransportMetrics = metrics.GRPCTransportMetrics
 
 type grpcMetricsRegistry struct {
 	dials          *expvar.Int
