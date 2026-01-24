ed82351e44d864b693b1e3ad6246574dde2aeb9b
feichai0017 <feichai0017@users.noreply.github.com>
2026-01-24 05:40:54 +0000

    deploy: 05d40aace703506f42aa36f8fe5dc6bad8b66d57

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d87e9139.js',
+                path_to_root + 'searchindex-b0b73f3c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
