a0de284e7a387eea26ae402ba4db40f2112c43a9
eric_song <songguocheng348@gmail.com>
2026-02-15 14:13:35 +0800

    refactor: unify file-layer IO with vfs and remove dead interfaces

--- file/file.go
+++ file/file.go
@@ -1,29 +1,14 @@
 // Package file provides low-level file and mmap primitives shared by WAL, vlog, and SST layers.
 package file
 
-import "io"
-
 import "github.com/feichai0017/NoKV/vfs"
 
-// Options
+// Options controls file opening parameters used by storage primitives.
 type Options struct {
 	FID      uint64
 	FileName string
 	Dir      string
-	Path     string
 	Flag     int
 	MaxSz    int
 	FS       vfs.FS
-}
-
-type CoreFile interface {
-	Close() error
-	Truncature(n int64) error
-	ReName(name string) error
-	NewReader(offset int) io.Reader
-	Bytes(off, sz int) ([]byte, error)
-	AllocateSlice(sz, offset int) ([]byte, int, error)
-	Sync() error
-	Delete() error
-	Slice(offset int) []byte
 }
