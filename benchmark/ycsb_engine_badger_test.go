6a145ef4435ce49c1ddcef171af693dd61e7be67
ByteByteUp <hangon2026@proton.me>
2026-02-19 17:47:33 +0800

    feat: add batch insert support for badger and pure insert workload

--- benchmark/ycsb_engine_badger_test.go
+++ benchmark/ycsb_engine_badger_test.go
@@ -1,6 +1,7 @@
 package benchmark
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -38,4 +39,40 @@ func TestBadgerEngineCRUD(t *testing.T) {
 	count, err := engine.Scan(key, 5)
 	require.NoError(t, err)
 	require.Equal(t, 1, count)
+}
+
+func TestBadgerBatchInsert(t *testing.T) {
+	engine := newBadgerEngine(ycsbEngineOptions{
+		BaseDir:           t.TempDir(),
+		ValueThreshold:    32,
+		SyncWrites:        false,
+		BlockCacheMB:      4,
+		BadgerCompression: "none",
+	})
+
+	require.Equal(t, "Badger", engine.Name())
+	require.NoError(t, engine.Open(true))
+	defer func() {
+		require.NoError(t, engine.Close())
+	}()
+
+	batchWriter, supportBatch := engine.(BatchWriter)
+	require.True(t, supportBatch)
+
+	keys := make([][]byte, 0, 1000000)
+	vals := make([][]byte, 0, 1000000)
+	for i := range 5 {
+		key := fmt.Sprintf("user%012d", i+1)
+		value := fmt.Sprintf("value%012d", i+1)
+		keys = append(keys, []byte(key))
+		vals = append(vals, []byte(value))
+	}
+	require.NoError(t, batchWriter.BatchInsert(keys, vals))
+
+	for i := range 5 {
+		key := fmt.Sprintf("user%012d", i+1)
+		out, err := engine.Read([]byte(key), nil)
+		require.NoError(t, err)
+		require.Equal(t, fmt.Sprintf("value%012d", i+1), string(out))
+	}
 }
