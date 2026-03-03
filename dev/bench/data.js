d1022796362d81546456e33fadc77d7a2ee64898
github-action-benchmark <github@users.noreply.github.com>
2026-03-03 04:50:41 +0000

    add Benchmark (go) benchmark result for 2eae6969f1b193ac1a3b115db8b427a6d58151ce

---
+++ dev/bench/data.js
@@ -0,0 +1,440 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1772513441050,
+  "repoUrl": "https://github.com/feichai0017/NoKV",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "songguocheng348@gmail.com",
+            "name": "feichai0017",
+            "username": "feichai0017"
+          },
+          "committer": {
+            "email": "songguocheng348@gmail.com",
+            "name": "feichai0017",
+            "username": "feichai0017"
+          },
+          "distinct": true,
+          "id": "2eae6969f1b193ac1a3b115db8b427a6d58151ce",
+          "message": "fix: release pooled redis entries and clarify SetEntry version semantics",
+          "timestamp": "2026-03-03T15:49:05+11:00",
+          "tree_id": "00a42032dd31685d4650ab6c2d15b364b06d530f",
+          "url": "https://github.com/feichai0017/NoKV/commit/2eae6969f1b193ac1a3b115db8b427a6d58151ce"
+        },
+        "date": 1772513439535,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkDBSetSmall (github.com/feichai0017/NoKV)",
+            "value": 7624,
+            "unit": "ns/op\t   4.20 MB/s\t     344 B/op\t      15 allocs/op",
+            "extra": "166471 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBSetSmall (github.com/feichai0017/NoKV) - ns/op",
+            "value": 7624,
+            "unit": "ns/op",
+            "extra": "166471 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBSetSmall (github.com/feichai0017/NoKV) - MB/s",
+            "value": 4.2,
+            "unit": "MB/s",
+            "extra": "166471 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBSetSmall (github.com/feichai0017/NoKV) - B/op",
+            "value": 344,
+            "unit": "B/op",
+            "extra": "166471 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBSetSmall (github.com/feichai0017/NoKV) - allocs/op",
+            "value": 15,
+            "unit": "allocs/op",
+            "extra": "166471 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBSetLarge (github.com/feichai0017/NoKV)",
+            "value": 13142,
+            "unit": "ns/op\t 311.68 MB/s\t     538 B/op\t      23 allocs/op",
+            "extra": "85822 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBSetLarge (github.com/feichai0017/NoKV) - ns/op",
+            "value": 13142,
+            "unit": "ns/op",
+            "extra": "85822 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBSetLarge (github.com/feichai0017/NoKV) - MB/s",
+            "value": 311.68,
+            "unit": "MB/s",
+            "extra": "85822 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBSetLarge (github.com/feichai0017/NoKV) - B/op",
+            "value": 538,
+            "unit": "B/op",
+            "extra": "85822 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBSetLarge (github.com/feichai0017/NoKV) - allocs/op",
+            "value": 23,
+            "unit": "allocs/op",
+            "extra": "85822 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBGetSmall (github.com/feichai0017/NoKV)",
+            "value": 7349,
+            "unit": "ns/op\t   8.71 MB/s\t   17579 B/op\t       8 allocs/op",
+            "extra": "1000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBGetSmall (github.com/feichai0017/NoKV) - ns/op",
+            "value": 7349,
+            "unit": "ns/op",
+            "extra": "1000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBGetSmall (github.com/feichai0017/NoKV) - MB/s",
+            "value": 8.71,
+            "unit": "MB/s",
+            "extra": "1000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBGetSmall (github.com/feichai0017/NoKV) - B/op",
+            "value": 17579,
+            "unit": "B/op",
+            "extra": "1000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBGetSmall (github.com/feichai0017/NoKV) - allocs/op",
+            "value": 8,
+            "unit": "allocs/op",
+            "extra": "1000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBGetLarge (github.com/feichai0017/NoKV)",
+            "value": 8784,
+            "unit": "ns/op\t 466.29 MB/s\t   25679 B/op\t      11 allocs/op",
+            "extra": "301308 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBGetLarge (github.com/feichai0017/NoKV) - ns/op",
+            "value": 8784,
+            "unit": "ns/op",
+            "extra": "301308 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBGetLarge (github.com/feichai0017/NoKV) - MB/s",
+            "value": 466.29,
+            "unit": "MB/s",
+            "extra": "301308 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBGetLarge (github.com/feichai0017/NoKV) - B/op",
+            "value": 25679,
+            "unit": "B/op",
+            "extra": "301308 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBGetLarge (github.com/feichai0017/NoKV) - allocs/op",
+            "value": 11,
+            "unit": "allocs/op",
+            "extra": "301308 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBBatchSet (github.com/feichai0017/NoKV)",
+            "value": 124759,
+            "unit": "ns/op\t 131.32 MB/s\t   56503 B/op\t     658 allocs/op",
+            "extra": "10000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBBatchSet (github.com/feichai0017/NoKV) - ns/op",
+            "value": 124759,
+            "unit": "ns/op",
+            "extra": "10000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBBatchSet (github.com/feichai0017/NoKV) - MB/s",
+            "value": 131.32,
+            "unit": "MB/s",
+            "extra": "10000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBBatchSet (github.com/feichai0017/NoKV) - B/op",
+            "value": 56503,
+            "unit": "B/op",
+            "extra": "10000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBBatchSet (github.com/feichai0017/NoKV) - allocs/op",
+            "value": 658,
+            "unit": "allocs/op",
+            "extra": "10000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBIteratorScan (github.com/feichai0017/NoKV)",
+            "value": 1620080,
+            "unit": "ns/op\t       1 B/op\t       0 allocs/op",
+            "extra": "715 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBIteratorScan (github.com/feichai0017/NoKV) - ns/op",
+            "value": 1620080,
+            "unit": "ns/op",
+            "extra": "715 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBIteratorScan (github.com/feichai0017/NoKV) - B/op",
+            "value": 1,
+            "unit": "B/op",
+            "extra": "715 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBIteratorScan (github.com/feichai0017/NoKV) - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "715 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBIteratorSeek (github.com/feichai0017/NoKV)",
+            "value": 568.1,
+            "unit": "ns/op\t      32 B/op\t       1 allocs/op",
+            "extra": "2236579 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBIteratorSeek (github.com/feichai0017/NoKV) - ns/op",
+            "value": 568.1,
+            "unit": "ns/op",
+            "extra": "2236579 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBIteratorSeek (github.com/feichai0017/NoKV) - B/op",
+            "value": 32,
+            "unit": "B/op",
+            "extra": "2236579 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDBIteratorSeek (github.com/feichai0017/NoKV) - allocs/op",
+            "value": 1,
+            "unit": "allocs/op",
+            "extra": "2236579 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkTouch (github.com/feichai0017/NoKV/hotring)",
+            "value": 30.84,
+            "unit": "ns/op",
+            "extra": "38648509 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkTouchParallel (github.com/feichai0017/NoKV/hotring)",
+            "value": 81.03,
+            "unit": "ns/op",
+            "extra": "14221076 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkTouchAndClamp (github.com/feichai0017/NoKV/hotring)",
+            "value": 24.11,
+            "unit": "ns/op",
+            "extra": "50351731 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFrequency (github.com/feichai0017/NoKV/hotring)",
+            "value": 14.25,
+            "unit": "ns/op",
+            "extra": "84398809 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkTopN (github.com/feichai0017/NoKV/hotring)",
+            "value": 20718288,
+            "unit": "ns/op",
+            "extra": "57 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSlidingWindow (github.com/feichai0017/NoKV/hotring)",
+            "value": 83.33,
+            "unit": "ns/op",
+            "extra": "14359672 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDecay (github.com/feichai0017/NoKV/hotring)",
+            "value": 73699,
+            "unit": "ns/op",
+            "extra": "16082 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLSMSetBatch (github.com/feichai0017/NoKV/lsm)",
+            "value": 46966,
+            "unit": "ns/op\t 174.42 MB/s\t   27638 B/op\t     454 allocs/op",
+            "extra": "25406 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLSMSetBatch (github.com/feichai0017/NoKV/lsm) - ns/op",
+            "value": 46966,
+            "unit": "ns/op",
+            "extra": "25406 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLSMSetBatch (github.com/feichai0017/NoKV/lsm) - MB/s",
+            "value": 174.42,
+            "unit": "MB/s",
+            "extra": "25406 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLSMSetBatch (github.com/feichai0017/NoKV/lsm) - B/op",
+            "value": 27638,
+            "unit": "B/op",
+            "extra": "25406 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLSMSetBatch (github.com/feichai0017/NoKV/lsm) - allocs/op",
+            "value": 454,
+            "unit": "allocs/op",
+            "extra": "25406 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLSMRotateFlush (github.com/feichai0017/NoKV/lsm)",
+            "value": 8814985,
+            "unit": "ns/op\t67521062 B/op\t    2563 allocs/op",
+            "extra": "139 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLSMRotateFlush (github.com/feichai0017/NoKV/lsm) - ns/op",
+            "value": 8814985,
+            "unit": "ns/op",
+            "extra": "139 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLSMRotateFlush (github.com/feichai0017/NoKV/lsm) - B/op",
+            "value": 67521062,
+            "unit": "B/op",
+            "extra": "139 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLSMRotateFlush (github.com/feichai0017/NoKV/lsm) - allocs/op",
+            "value": 2563,
+            "unit": "allocs/op",
+            "extra": "139 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkVLogAppendEntries (github.com/feichai0017/NoKV/vlog)",
+            "value": 23908,
+            "unit": "ns/op\t 342.64 MB/s\t    1794 B/op\t      35 allocs/op",
+            "extra": "102012 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkVLogAppendEntries (github.com/feichai0017/NoKV/vlog) - ns/op",
+            "value": 23908,
+            "unit": "ns/op",
+            "extra": "102012 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkVLogAppendEntries (github.com/feichai0017/NoKV/vlog) - MB/s",
+            "value": 342.64,
+            "unit": "MB/s",
+            "extra": "102012 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkVLogAppendEntries (github.com/feichai0017/NoKV/vlog) - B/op",
+            "value": 1794,
+            "unit": "B/op",
+            "extra": "102012 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkVLogAppendEntries (github.com/feichai0017/NoKV/vlog) - allocs/op",
+            "value": 35,
+            "unit": "allocs/op",
+            "extra": "102012 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkVLogReadValue (github.com/feichai0017/NoKV/vlog)",
+            "value": 176.3,
+            "unit": "ns/op\t1452.16 MB/s\t     272 B/op\t       2 allocs/op",
+            "extra": "7146988 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkVLogReadValue (github.com/feichai0017/NoKV/vlog) - ns/op",
+            "value": 176.3,
+            "unit": "ns/op",
+            "extra": "7146988 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkVLogReadValue (github.com/feichai0017/NoKV/vlog) - MB/s",
+            "value": 1452.16,
+            "unit": "MB/s",
+            "extra": "7146988 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkVLogReadValue (github.com/feichai0017/NoKV/vlog) - B/op",
+            "value": 272,
+            "unit": "B/op",
+            "extra": "7146988 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkVLogReadValue (github.com/feichai0017/NoKV/vlog) - allocs/op",
+            "value": 2,
+            "unit": "allocs/op",
+            "extra": "7146988 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkWALAppend (github.com/feichai0017/NoKV/wal)",
+            "value": 618.2,
+            "unit": "ns/op\t 414.09 MB/s\t      36 B/op\t       5 allocs/op",
+            "extra": "4298809 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkWALAppend (github.com/feichai0017/NoKV/wal) - ns/op",
+            "value": 618.2,
+            "unit": "ns/op",
+            "extra": "4298809 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkWALAppend (github.com/feichai0017/NoKV/wal) - MB/s",
+            "value": 414.09,
+            "unit": "MB/s",
+            "extra": "4298809 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkWALAppend (github.com/feichai0017/NoKV/wal) - B/op",
+            "value": 36,
+            "unit": "B/op",
+            "extra": "4298809 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkWALAppend (github.com/feichai0017/NoKV/wal) - allocs/op",
+            "value": 5,
+            "unit": "allocs/op",
+            "extra": "4298809 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkWALReplay (github.com/feichai0017/NoKV/wal)",
+            "value": 2038413,
+            "unit": "ns/op\t 3064040 B/op\t   40018 allocs/op",
+            "extra": "585 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkWALReplay (github.com/feichai0017/NoKV/wal) - ns/op",
+            "value": 2038413,
+            "unit": "ns/op",
+            "extra": "585 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkWALReplay (github.com/feichai0017/NoKV/wal) - B/op",
+            "value": 3064040,
+            "unit": "B/op",
+            "extra": "585 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkWALReplay (github.com/feichai0017/NoKV/wal) - allocs/op",
+            "value": 40018,
+            "unit": "allocs/op",
+            "extra": "585 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
