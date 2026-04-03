12f760e40b2afa644cda498334aa6db3942f4e4a
eric_song <songguocheng348@gmail.com>
2026-04-03 11:04:07 +0800

    refactor: remove metadata root raft backend

--- meta/root/raft/errors.go
+++
@@ -1,9 +0,0 @@
-package rootraft
-
-import "errors"
-
-var (
-	ErrNotLeader       = errors.New("meta/root/raft: node is not leader")
-	ErrNoTransport     = errors.New("meta/root/raft: transport is required for remote delivery")
-	ErrUnsupportedType = errors.New("meta/root/raft: unsupported committed entry type")
-)
