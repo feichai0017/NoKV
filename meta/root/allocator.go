ec62a2693097cde091676d73d44efbbd5e025077
eric_song <songguocheng348@gmail.com>
2026-04-03 17:08:44 +0800

    refactor: rename metadata root facade interfaces

--- meta/root/allocator.go
+++ meta/root/allocator.go
@@ -1,7 +1,5 @@
 package root
 
-import rootstate "github.com/feichai0017/NoKV/meta/root/state"
-
 // AllocatorKind identifies one globally fenced allocator domain.
 type AllocatorKind uint8
 
@@ -10,6 +8,3 @@ const (
 	AllocatorKindID
 	AllocatorKindTSO
 )
-
-type Cursor = rootstate.Cursor
-type CommitInfo = rootstate.CommitInfo
