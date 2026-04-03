76372c67f21ace87cecb239037697deedaa0c9e0
eric_song <songguocheng348@gmail.com>
2026-04-03 13:17:12 +0800

    refactor: reorganize metadata root package boundaries

---
+++ meta/root/descriptor_view.go
@@ -0,0 +1,49 @@
+package root
+
+import (
+	"sort"
+
+	"github.com/feichai0017/NoKV/raftstore/descriptor"
+)
+
+// ApplyEventToDescriptors applies one rooted topology event into a materialized
+// descriptor catalog.
+func ApplyEventToDescriptors(descriptors map[uint64]descriptor.Descriptor, event Event) {
+	if descriptors == nil {
+		return
+	}
+	switch {
+	case event.RegionDescriptor != nil:
+		descriptors[event.RegionDescriptor.Descriptor.RegionID] = event.RegionDescriptor.Descriptor.Clone()
+	case event.RegionRemoval != nil:
+		delete(descriptors, event.RegionRemoval.RegionID)
+	case event.RangeSplit != nil:
+		delete(descriptors, event.RangeSplit.ParentRegionID)
+		descriptors[event.RangeSplit.Left.RegionID] = event.RangeSplit.Left.Clone()
+		descriptors[event.RangeSplit.Right.RegionID] = event.RangeSplit.Right.Clone()
+	case event.RangeMerge != nil:
+		delete(descriptors, event.RangeMerge.LeftRegionID)
+		delete(descriptors, event.RangeMerge.RightRegionID)
+		descriptors[event.RangeMerge.Merged.RegionID] = event.RangeMerge.Merged.Clone()
+	case event.PeerChange != nil:
+		descriptors[event.PeerChange.Region.RegionID] = event.PeerChange.Region.Clone()
+	}
+}
+
+// SnapshotDescriptorEvents materializes descriptor truth into a stable event
+// sequence for bootstrap/recovery callers.
+func SnapshotDescriptorEvents(descs map[uint64]descriptor.Descriptor) []Event {
+	if len(descs) == 0 {
+		return nil
+	}
+	ids := make([]uint64, 0, len(descs))
+	for id := range descs {
+		ids = append(ids, id)
+	}
+	sort.Slice(ids, func(i, j int) bool { return ids[i] < ids[j] })
+	events := make([]Event, 0, len(ids))
+	for _, id := range ids {
+		events = append(events, RegionDescriptorPublished(descs[id]))
+	}
+	return events
+}
