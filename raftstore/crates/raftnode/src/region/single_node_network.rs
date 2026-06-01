62c399d9fb1e1d481fa92f6688c1a3f2b42f931d
eric_song <songguocheng348@gmail.com>
2026-06-01 10:00:08 +1000

    refactor: split raft region api modules
    Signed-off-by: eric_song <songguocheng348@gmail.com>

---
+++ raftstore/crates/raftnode/src/region/single_node_network.rs
@@ -0,0 +1,67 @@
+use std::error::Error as StdError;
+
+use openraft::{
+    error::{RPCError, RaftError, Unreachable},
+    network::{RPCOption, RaftNetwork, RaftNetworkFactory},
+};
+
+use crate::{BasicNode, NodeId, RaftStoreConfig};
+
+#[derive(Clone, Default)]
+pub(super) struct NoopNetworkFactory;
+
+pub(super) struct NoopNetwork;
+
+#[derive(Debug, thiserror::Error)]
+#[error("single-node raft network has no remote peers")]
+struct NoopNetworkError;
+
+impl RaftNetworkFactory<RaftStoreConfig> for NoopNetworkFactory {
+    type Network = NoopNetwork;
+
+    async fn new_client(&mut self, _target: NodeId, _node: &BasicNode) -> Self::Network {
+        NoopNetwork
+    }
+}
+
+impl RaftNetwork<RaftStoreConfig> for NoopNetwork {
+    async fn append_entries(
+        &mut self,
+        _rpc: openraft::raft::AppendEntriesRequest<RaftStoreConfig>,
+        _option: RPCOption,
+    ) -> Result<
+        openraft::raft::AppendEntriesResponse<NodeId>,
+        RPCError<NodeId, BasicNode, RaftError<NodeId>>,
+    > {
+        Err(remote_unreachable())
+    }
+
+    async fn install_snapshot(
+        &mut self,
+        _rpc: openraft::raft::InstallSnapshotRequest<RaftStoreConfig>,
+        _option: RPCOption,
+    ) -> Result<
+        openraft::raft::InstallSnapshotResponse<NodeId>,
+        RPCError<NodeId, BasicNode, RaftError<NodeId, openraft::error::InstallSnapshotError>>,
+    > {
+        Err(remote_unreachable())
+    }
+
+    async fn vote(
+        &mut self,
+        _rpc: openraft::raft::VoteRequest<NodeId>,
+        _option: RPCOption,
+    ) -> Result<openraft::raft::VoteResponse<NodeId>, RPCError<NodeId, BasicNode, RaftError<NodeId>>>
+    {
+        Err(remote_unreachable())
+    }
+}
+
+fn remote_unreachable<NID, N, E>() -> RPCError<NID, N, E>
+where
+    NID: openraft::NodeId,
+    N: openraft::Node,
+    E: StdError,
+{
+    RPCError::Unreachable(Unreachable::new(&NoopNetworkError))
+}
